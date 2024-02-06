adc33339a9d1d0c67e43534e6c6ae455dee4aa18
PourthieAlexis <alexispourthie@gmail.com>
2024-02-06 13:23:09 +0100

    Add list question

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Author;
+use App\Entity\Comment;
 use App\Entity\Question;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -10,25 +12,28 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $questionsData = [
-            [
-                'title' => 'Première question',
-                'content' => 'Contenu de la première question...',
-            ],
-            [
-                'title' => 'Deuxième question',
-                'content' => 'Contenu de la deuxième question...',
-            ],
-            [
-                'title' => 'Troisième question',
-                'content' => 'Contenu de la troisième question...',
-            ],
-        ];
+        // Création de l'auteur
+        $author = new Author();
+        $author->setName("John Doe");
+        $author->setProfileImage("https://randomuser.me/api/portraits/men/62.jpg");
 
-        foreach ($questionsData as $data) {
+        $manager->persist($author);
+
+        // Création de questions avec des commentaires
+        for ($i = 0; $i < 5; $i++) {
             $question = new Question();
-            $question->setTitle($data['title']);
-            $question->setContent($data['content']);
+            $question->setTitle("Titre de la question $i");
+            $question->setContent("Contenu de la question $i");
+            $question->setVote(rand(0, 20));
+            $question->setAuthor($author);
+
+            // Création de commentaires pour chaque question
+            for ($j = 0; $j < rand(0, 5); $j++) {
+                $comment = new Comment();
+                $comment->setContent("Commentaire $j pour la question $i");
+                $comment->setQuestion($question);
+                $manager->persist($comment);
+            }
 
             $manager->persist($question);
         }
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\QuestionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: QuestionRepository::class)]
@@ -18,6 +20,21 @@ class Question
 
     #[ORM\Column(length: 255)]
     private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'questions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Author $author = null;
+
+    #[ORM\OneToMany(mappedBy: 'question', targetEntity: Comment::class)]
+    private Collection $comments;
+
+    #[ORM\Column]
+    private ?int $vote = null;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +61,60 @@ class Question
     public function setContent(string $content): static
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getQuestion() === $this) {
+                $comment->setQuestion(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getVote(): ?int
+    {
+        return $this->vote;
+    }
+
+    public function setVote(int $vote): static
+    {
+        $this->vote = $vote;
 
         return $this;
     }
